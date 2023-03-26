d985f110f680be612bea3cadcc7c64d2c42a176a
niceneo <niceneo666@gmail.com>
2023-03-26 18:09:55 +0800

    Read me

---
+++ command/command_test.go
@@ -0,0 +1,13 @@
+package command
+
+import "testing"
+
+func TestCommand_Execute(t *testing.T) {
+	laowang := NewPerson("wang",NewCommand(nil,nil))
+	laozhang:= NewPerson("zhang",NewCommand(&laowang,laowang.Listen))
+	laofeng:=NewPerson("feng",NewCommand(&laozhang,laozhang.Buy))
+	laoding:=NewPerson("ding",NewCommand(&laofeng,laofeng.Cook))
+	laoli:=NewPerson("li",NewCommand(&laoding,laoding.Wash))
+
+	laoli.Talk()
+}
